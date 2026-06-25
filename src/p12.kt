ab687f7f06bb74b756a90ff4bdb6be2ded8e611b
maharsh-patel <maharshpatel1306@gmail.com>
2026-06-25 14:23:36 +0530

    init

---
+++ src/p12.kt
@@ -0,0 +1,18 @@
+fun main(){
+    val i = 30000
+    val j = i.toDouble()
+    println("i = $1")
+    println("j = $j")
+
+    val s1 = "30500"
+    val a = s1.toInt()
+    println("s1 = $s1")
+    println("a = $a")
+
+    val s2 = "4005500"
+    val b = s2.toDouble()
+    println("s2 = $s2")
+    println("b = $b")
+
+}
+
