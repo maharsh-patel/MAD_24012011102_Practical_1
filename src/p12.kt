aac3b74e3e8a318ad98aa1ac747515ef2b84d0f3
maharsh-patel <maharshpatel1306@gmail.com>
2026-06-25 14:28:51 +0530

    print statement update

--- src/p12.kt
+++ src/p12.kt
@@ -1,18 +1,19 @@
 fun main(){
     val i = 30000
     val j = i.toDouble()
-    println("i = $1")
-    println("j = $j")
+    println("Integer Value:$i")
+    println("Double Value (From Integer):$j")
 
     val s1 = "30500"
     val a = s1.toInt()
-    println("s1 = $s1")
-    println("a = $a")
+    println("String Value:$s1")
+    println("Integer Value1 (From String):$a")
+
 
     val s2 = "4005500"
     val b = s2.toDouble()
-    println("s2 = $s2")
-    println("b = $b")
+    println("String Value:$s2")
+    println("Double Value (From String):$b")
 
 }
 
