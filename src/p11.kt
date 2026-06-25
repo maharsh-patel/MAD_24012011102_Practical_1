526cad0b5fc2cd2625afb5d626b8381cad3e10b1
maharsh-patel <maharshpatel1306@gmail.com>
2026-06-25 14:07:21 +0530

    init

---
+++ src/p11.kt
@@ -0,0 +1,45 @@
+fun main(){
+    var i = 200
+    println("i=" + i)
+
+    var j : Int = 100
+    println("j=" + j)
+
+    var k : Float = 1.5f
+    println("k=" + k)
+
+    var l : Char = 'M'
+    println("l=" + l)
+
+    var m : String = "Maharsh"
+    println("m=" + m)
+
+    var n : Boolean = false
+    println("n=" + n)
+
+    var o : Double = 13.06
+    println("o=" + o)
+
+    var p : Long = 130605
+    println("p=" + p)
+
+    var q : Short = -17
+    println("q=" + q)
+
+    var r : Byte = 127
+
+    val z = 20000
+
+    println("z=" + z)
+    println("r=" + r)
+
+
+
+
+
+
+
+
+}
+
+
