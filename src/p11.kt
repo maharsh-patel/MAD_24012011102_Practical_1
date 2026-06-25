da0adfb0753ad20f208dc1913a2dfc293f3cbac7
maharsh-patel <maharshpatel1306@gmail.com>
2026-06-25 14:12:03 +0530

    print msg update

--- src/p11.kt
+++ src/p11.kt
@@ -1,45 +1,34 @@
-fun main(){
+fun main() {
     var i = 200
-    println("i=" + i)
+    println("Integer value i = " + i)
 
     var j : Int = 100
-    println("j=" + j)
+    println("Integer value j = " + j)
 
     var k : Float = 1.5f
-    println("k=" + k)
+    println("Float value k = " + k)
 
     var l : Char = 'M'
-    println("l=" + l)
+    println("Character value l = " + l)
 
     var m : String = "Maharsh"
-    println("m=" + m)
+    println("String value m = " + m)
 
     var n : Boolean = false
-    println("n=" + n)
+    println("Boolean value n = " + n)
 
     var o : Double = 13.06
-    println("o=" + o)
+    println("Double value o = " + o)
 
     var p : Long = 130605
-    println("p=" + p)
+    println("Long value p = " + p)
 
     var q : Short = -17
-    println("q=" + q)
+    println("Short value q = " + q)
 
     var r : Byte = 127
+    println("Byte value r = " + r)
 
     val z = 20000
-
-    println("z=" + z)
-    println("r=" + r)
-
-
-
-
-
-
-
-
+    println("Constant integer value z = " + z)
 }
-
-
