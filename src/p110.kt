afec86caec73e1a06f68fffa57772201483deb2a
maharsh-patel <maharshpatel1306@gmail.com>
2026-07-09 14:05:15 +0530

    print statement update

--- src/p110.kt
+++ src/p110.kt
@@ -1,21 +1,25 @@
-class Car(m: String) {
+//primary constructor
 
-    var model: String = ""
+
+open class Car(var model: String) {
+
     var price: Double = 0.0
-
-    init{
-        model = m
-    }
-
     constructor(p:Double , m:String) : this(m){
         price = p
     }
 
 }
 
+class Suzuki (m: String) : Car(m)
+
 fun main() {
+
     val c1 = Car( 2500000.00, "BMW")
+    val c2 = Suzuki(  "Grand Vttara")
 
     println(c1.model)
     println(c1.price)
+
+    println(c2.model)
+    println(c2.price)
 }
